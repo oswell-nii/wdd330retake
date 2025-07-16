21bab29bec2ed31b02ef7fa2faae11aa9757b1a5
oswell-oxy <oswellokine1999@gmail.com>
2025-07-16 18:08:03 +0000

    testing few changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 // main.js
-import { loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
